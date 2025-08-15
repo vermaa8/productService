a6992d7dd58e4aaed5b7153388ff9326a1e2f0dc
vermaa8 <ashish.verma1985@gmail.com>
2025-08-15 07:43:46 +0530

    Bug fixes in userservice

--- src/main/java/com/scaler/capstone/productservice/ProductService/configuration/SecurityConfig.java
+++ src/main/java/com/scaler/capstone/productservice/ProductService/configuration/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.scaler.capstone.productservice.ProductService.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,7 +21,7 @@ public class SecurityConfig {
 //    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
--- src/main/java/com/scaler/capstone/productservice/ProductService/service/UserService.java
+++ src/main/java/com/scaler/capstone/productservice/ProductService/service/UserService.java
@@ -4,8 +4,8 @@ import com.scaler.capstone.productservice.ProductService.models.Token;
 import com.scaler.capstone.productservice.ProductService.models.User;
 
 public interface UserService {
-    public User signUp(String name, String email, String password);
-    public Token login(String email, String password);
-    public User validate(String token);
-    public void logout(String token);
+    User signUp(String name, String email, String password);
+    Token login(String email, String password);
+    User validate(String token);
+    void logout(String token);
 }
